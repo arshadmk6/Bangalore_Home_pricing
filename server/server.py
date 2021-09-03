8128953d2ba9a9216b76a5ed0173db5bb7d72167
Arshad M K <arshuarshad.me@gmail.com>
2021-09-03 12:26:17 +0530

    First Commit

--- server/server.py
+++ server/server.py
@@ -3,18 +3,16 @@ import util
 
 app = Flask(__name__)
 
-
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
-    response.headers.add('Access_Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
-
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -22,15 +20,13 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
-
-    response.headers.add('Access_Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
-
 if __name__ == "__main__":
-    print("Starting Python Flask Server for Home Price Prediction")
-    app.debug = True
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
     app.run()
